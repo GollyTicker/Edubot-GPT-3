432e89c726f1ec602ff8472b7dcc4ae433232bca
GollyTicker <golly.ticker@gmail.com>
2021-03-06 12:07:49 +0100

    improved interactions. sending atmost 4 questions from history

--- first.py
+++ first.py
@@ -1,20 +1,51 @@
-
+import logging
 import openai
+from init import init
+import json
+
+logging.basicConfig(filename='first.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 with open('.key', 'r') as file:
     openai.api_key = file.read().replace('\n', '')
 
-init_prompt = ""
-with open('prompt-edu-chat.txt', 'r') as file:
-    init_prompt = file.read()
-
-print("Ask a question or a topic you are interested to learn about: ", end = "")
 question = 0
-prompt = init_prompt
-while question not in [".", ""] :
+prompts = init
+
+gpt3options = {
+  "max_tokens": 200,
+  "temperature": 0.7,
+  "stop": "Q. ",
+}
+
+def to_multiline_string(prompts):
+    return "\n".join([p for p in prompts])
+
+def clean_newlines(response):
+    if response[:1] == "\n":
+        response = response[1:]
+    if response[-1:] == "\n":
+        response = response[:-1]
+    return response
+
+print("Ask a question or a topic you are interested to learn about. (Write . to quit.)")
+while True:
+    print("Q. ", end = "")
     question = input()
-    prompt = prompt + "\nQ. " + question + "\n "
-    response_object = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=100, temperature = 0.7, stop = "Q. ")
-    response = response_object["choices"][0]["text"]
+    if question in [".",""]:
+        break
+    prompts.append("Q. " + question)
+
+    response_object = openai.Completion.create(engine="davinci", prompt=to_multiline_string(prompts), **gpt3options)
+
+    logging.info(f"Response: {json.dumps(response_object,indent=2)}")
+
+    response = clean_newlines(response_object["choices"][0]["text"])
     print(response)
-    prompt = prompt + response
+    prompts.append(response)
+
+    # remove oldest question answer pair
+    if len(prompts) > 2 * 4:
+        prompts.pop(0)
+        prompts.pop(0)
+
+print("Ended conversation.")
